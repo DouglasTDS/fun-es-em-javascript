74e073b3b4be04dd09f3fa2c1450b8ca2061df49
DouglasDiniz <douglas.alunoti@gmail.com>
2026-05-20 21:17:09 -0300

    Aula Dom

---
+++ script (3).js
@@ -0,0 +1,5 @@
+function mudarTexto() {
+  // Vai no HTML e pega o elemento pelo id .innerHTML = Altera o HTML para o que eu escrever aqui
+  document.getElementById("texto").innerHTML =
+    "Texto novo ao clicar no botào :D";
+}
